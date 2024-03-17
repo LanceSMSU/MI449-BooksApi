5091ad7b966810ad4efca639ecc0a3240ffb0b27
LanceStemple <stemplel@msu.edu>
2024-03-17 16:40:37 -0400

    Fixed the issue of table not appearing and styled table

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.7.1"
 
 const supabaseUrl = 'https://nnpcyzeigjxgcifkctlj.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucGN5emVpZ2p4Z2NpZmtjdGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3MDQ0NzUsImV4cCI6MjAyNjI4MDQ3NX0.GkI50APl0MiecQDBkIP2LlnfXmugOTWxg3IWTWWh2Mo'
@@ -14,6 +14,6 @@ async function getBooks() {
        let bookList = document.getElementById('books');
        bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td></tr>`;
      }
-  }
+}
 
 getBooks();
